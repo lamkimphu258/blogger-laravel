091f1a6f54cf9245a86b53a88460d3e235bf69fe
lamkimphu258 <lamkimphu258@gmail.com>
2021-07-02 09:02:56 +0700

    Add base layout

---
+++ app/resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="text-center text-lg-start bg-light text-muted position-absolute bottom-0 start-0 end-0">
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+        © {{ date('Y') }} Copyright:
+        <a class="text-reset fw-bold" href="{{ route('home') }}">Blogger Laravel</a>
+    </div>
+</footer>
+
--- app/routes/web.php
+++ app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommonController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('/')->group(function () {
+    Route::get('', [CommonController::class, 'home'])->name('home');
 });
