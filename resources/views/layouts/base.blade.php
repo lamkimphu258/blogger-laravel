4dcb75a7380d223d5b5a7dbe54ead443a1c2a4c3
lamkimphu258 <lamkimphu258@gmail.com>
2021-08-13 23:45:38 +0700

    Change assest to secure

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-    <script src="{{ asset('js/bootstrap.bundle.js') }}"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
+    <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/bootstrap.css') }}">
+    <script src="{{ secure_asset('js/bootstrap.bundle.js') }}"></script>
+    <script src="{{ secure_asset('js/app.js') }}"></script>
     <script src="https://kit.fontawesome.com/d6903fe8c7.js" crossorigin="anonymous"></script>
     <script src="//cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
     <title>@yield('title') - Blogger</title>
